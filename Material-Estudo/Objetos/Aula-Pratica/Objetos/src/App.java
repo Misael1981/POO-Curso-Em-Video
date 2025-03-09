82e19f16b162bc6050119393bc62d36343430e6e
Misael Borges <ml_bbd@hotmail.com>
2025-03-09 10:25:26 -0300

    Aula Prática Objetos

--- Material-Estudo/Objetos/Aula-Pratica/Objetos/src/App.java
+++ Material-Estudo/Objetos/Aula-Pratica/Objetos/src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Caneta c1 = new Caneta();
+        c1.modelo = "Bic";
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.carga = 50;
+
+        c1.tampar();
+        c1.status();
+        c1.rabiscar();
+
+        System.out.println("============");
+
+        Caneta c2 = new Caneta();
+        c2.modelo = "Pilot";
+        c2.cor = "Vermelha";
+        c2.ponta = 1f;
+        c2.carga = 10;
+
+        c2.destampar();
+        c2.status();
+        c2.rabiscar();
     }
 }
