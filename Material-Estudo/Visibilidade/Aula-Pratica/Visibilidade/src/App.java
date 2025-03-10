90199a096ec317aa4b16c9aed0a5a5b2c35fe9e7
Misael Borges <ml_bbd@hotmail.com>
2025-03-10 09:08:02 -0300

    Finalizando Visibilidade

--- Material-Estudo/Visibilidade/Aula-Pratica/Visibilidade/src/App.java
+++ Material-Estudo/Visibilidade/Aula-Pratica/Visibilidade/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Caneta c1 = new Caneta();
+        c1.carga = 80;
+        c1.status();
     }
 }
